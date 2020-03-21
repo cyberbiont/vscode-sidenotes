a19518f313a5d8566cb37efb3b1cce2ac47a901c
cyberbiont <cyberbiont@gmail.com>
2020-03-21 17:07:53 +0200

    includes in watch all subfolders in sidenotes folder

--- src/lib/changeTracker/vscodeFileSystemWatcherMaker.ts
+++ src/lib/changeTracker/vscodeFileSystemWatcherMaker.ts
@@ -42,7 +42,7 @@ export default class VSCodeFileSystemWatcherMaker extends FileChangeTracker {
 		return new RelativePattern(
 			this.getFullPathToSubfolder(workspace),
 			// `*${this.cfg.storage.files.defaultContentFileExtension}`
-			'*.*',
+			'**\/*.*',
 		);
 	}
 
